7c6c1da4596324e59be9a3f2ee9e434af8417eb2
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2024-11-27 16:05:01 -0300

    feat: add tailwind and flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PruebaCorta2_IDWM';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
